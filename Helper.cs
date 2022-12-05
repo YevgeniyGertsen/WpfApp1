686c35e653cc982c5c3e579e9f9b6cd69ffc1187
gersen.e.a <gersen.e.a@gmail.com>
2022-12-05 21:41:30 +0600

    Добавьте файлы проекта.

---
+++ Helper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Helper
+    {
+        public static string SenttMsg(string to,
+            string subject, string body, out bool result)
+        {
+            try
+            {
+                MailMessage msg = new MailMessage();
+            msg.From = new MailAddress("gersen.e.a@gmail.com",
+                "Yevgeniy Gertsen");
+
+            msg.To.Add(to);
+            //msg.CC.Add("");
+            msg.Subject = subject;
+            msg.Priority = MailPriority.High;
+            msg.Body = body;
+            //msg.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            //smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("login", "password");
+
+          
+                smtp.Send(msg);
+                result = true;
+                return "Сообщение отправлено";
+            }
+            catch (Exception ex)
+            {
+                result = false;
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+           
+
+        }
+
+        private void btnSentMsg_Click(object sender, RoutedEventArgs e)
+        {
+            bool result = false;
+            lblMessage.Content = Helper.SenttMsg(tbxTo.Text,
+                tbxSubject.Text,
+                tbxBody.Text, 
+                out result);
+
+            if(!result)
+            {              
+                lblMessage.Foreground = new SolidColorBrush(Colors.Red);
+            }
+            else
+            {               
+                lblMessage.Foreground = new SolidColorBrush(Colors.Green);
+            }
+        }
+    }
+}
